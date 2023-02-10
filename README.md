969e602f48358cfacb6d883710045ab595fbc358
a.shakhovnina <a.shakhovnina@iponweb.net>
2023-02-10 14:46:16 +0200

    dockerirze app and functional tests

--- README.md
+++ README.md
@@ -3,4 +3,15 @@
 ```
 cd webapp
 pip install -r requirements.txt
-uvicorn main:app --reload --log-config="log.ini" ```
+uvicorn main:app --reload --log-config="log.ini"
+```
+
+run web add in container:
+
+cd webapp
+
+docker build -t webappimage .
+
+docker run -d --name webapp -p 80:80 webappimage
+
+export TEST_APP_URL=http://localhost
--- webapp/main.py
+++ webapp/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 
 app = FastAPI()
 
@@ -7,28 +7,33 @@ def server_response() -> str:
     return {"field1": "same json", "field2": "for any ID passed"}
 
 
-@app.get("/people/{person_id}/", status_code=200)
-def get_person(person_id: int):
-    if person_id not in range(1, 100):
+def validate_id(element_id) -> None:
+    try:
+        element_id_int = int(element_id)
+    except:
         raise HTTPException(
-            status_code=404, detail=f"Person with ID {person_id} not found"
+            status_code=400,
+            detail=f"Incorrect element ID: {element_id}. The value should be integer"
         )
+    if element_id_int not in range(1, 100):
+        raise HTTPException(
+            status_code=404, detail=f"Record with ID {element_id_int} not found"
+        )
+
+
+@app.get("/people/{person_id}/", status_code=status.HTTP_200_OK)
+def get_person(person_id):
+    validate_id(person_id)
     return server_response()
 
 
 @app.get("/planets/{planet_id}/", status_code=200)
-def get_planet(planet_id: int):
-    if planet_id not in range(1, 100):
-        raise HTTPException(
-            status_code=404, detail=f"Planet with ID {planet_id} not found"
-        )
+def get_planet(planet_id):
+    validate_id(planet_id)
     return server_response()
 
 
 @app.get("/starships/{starship_id}/", status_code=200)
-def get_starship(starship_id: int):
-    if starship_id not in range(1, 100):
-        raise HTTPException(
-            status_code=404, detail=f"Starship with ID {starship_id} not found"
-        )
+def get_starship(starship_id):
+    validate_id(starship_id)
     return server_response()
